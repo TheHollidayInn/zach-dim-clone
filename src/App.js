3e7b89946c83e74060bfbc27f63fa69f4c64199c
Keith Holliday <krh121791@yahoo.com>
2022-04-24 15:44:59 -0600

    Updated the Main Title.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Inventory from "./containers/Inventory";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello Zac's Clone of DIM</h1>
       <h2>Start editing to see some magic happen!</h2>
 
       <Inventory />
